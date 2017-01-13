f366fd7a3b5db474f4abd5ad42bceb739de4f042
Polaken <polachgg@gmail.com>
2017-01-13 13:57:17 -0300

    asdasdasd
    asdasd

--- includes/dbb.php
+++ includes/dbb.php
@@ -1,2 +1,8 @@
-<?php 
+<?php
+function crearConexion() {
+	$config = parse_ini_file("database.ini");
+	$con = new mysqli ($config['SERVER'],$config['USER'],$config['PASSWORD'],$config['NAMEBDD']);
+	if ($con->connect_errno > 0)
+	die ( "Error en la conexión" );
+	return $con;
  ?>
--- index.php
+++ index.php
@@ -10,15 +10,15 @@
     <meta name="Andres Plehm" content="Clasificados">
     <link rel="icon" href="images\escudo_sm.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	
 </head>
 <body>
+<div class="container">
 <header>
 	<img src="images\escudo_sm.png" alt="Escudo de San Martin en los Andes">
 	<h1>Clasificados San Martin</h1>
 	<p>De todo un poco y <b>mucho tambien</b>....</p>
 </header>
-<div><!-- Cuerpo de pagina-->
+<div class="container"><!-- Cuerpo de pagina-->
 <div> <!-- Barra de navegación-->
 	<nav>
 		<ul>
@@ -28,6 +28,7 @@
 			<a href="">Remiserias</a>
 			<a href="">Centros de paseo</a>
 			<a href="">Eventos</a>
+			<a href="">Empleos</a>
 		</ul>
 	</nav>
 </div>
@@ -39,6 +40,7 @@
 	<p>&copy Todos los derechos reservados <b>Andres Plehm</b> <?php echo FECHA; ?></p>
 	<a href="login.php">x</a>
 </footer>
+</div>
 
 	<script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
   	crossorigin="anonymous"></script>
